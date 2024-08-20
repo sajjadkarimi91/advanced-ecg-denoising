ef05e3136f83dedd695e7a5cfdaedeaf9953b049
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2024-08-20 22:47:59 +0330

    Create oset_denoising.m

---
+++ oset_denoising.m
@@ -0,0 +1,7 @@
+clear;
+close all;
+clc;
+
+
+oset_path = 'D:\projects\toolboxes\OSET'; 'D:\my_projects\Alphanumeric\multimodal-cardiac-biomarkers\OSET';% enter the path of OSET toolbox
+addpath(genpath(oset_path))
