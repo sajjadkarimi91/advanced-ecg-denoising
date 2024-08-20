302a59412736bd30d130d464cd0774191d1cc6d2
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2024-08-20 22:47:27 +0330

    making simulated noisy data from real LU database

--- make_local_lu.m
+++ make_local_lu.m
@@ -23,7 +23,6 @@ for m = 1:length(local_db_files)
     load([db_folder,'/',in_fname,'.mat']);
 
 
-
     for ch = 1:12
 
         index_R =  true_position(ch).R;
@@ -36,23 +35,6 @@ for m = 1:length(local_db_files)
         ecg_rpeaks_index_p = index_R;
         ecg_rpeaks_index_p(isnan(ecg_rpeaks_index_p))=[];
 
-        ecg_QRSon_true = true_position(ch).QRSon; ecg_QRSon_wavdet_p = ecg_QRSon_true;  ecg_QRSon_wavdet_p(isnan(ecg_QRSon_wavdet_p))=[];
-
-
-        ecg_QRSon_true = true_position(ch).QRSon; ecg_QRSon_wavdet_p = ecg_QRSon_true;  ecg_QRSon_wavdet_p(isnan(ecg_QRSon_wavdet_p))=[];
-
-        ecg_QRSoff_true = true_position(ch).QRSoff; ecg_QRSoff_wavedet_p = ecg_QRSoff_true;  ecg_QRSoff_wavedet_p(isnan(ecg_QRSoff_wavedet_p))=[];
-
-        ecg_Pon_index = true_position(ch).Pon;  ecg_Pon_index_p = ecg_Pon_index; ecg_Pon_index_p(isnan(ecg_Pon_index_p))=[];
-        ecg_P_index = true_position(ch).P;  ecg_P_index_p = ecg_P_index; ecg_P_index_p(isnan(ecg_P_index_p))=[];
-        ecg_Poff_index = true_position(ch).Poff;  ecg_Poff_index_p = ecg_Poff_index; ecg_Poff_index_p(isnan(ecg_Poff_index_p))=[];
-
-        ecg_Ton_true = true_position(ch).Ton';     ecg_Ton_wavedet_p = ecg_Ton_true;  ecg_Ton_wavedet_p(isnan(ecg_Ton_wavedet_p))=[];
-
-        ecg_T_true = true_position(ch).T';     ecg_T_wavedet_p = ecg_T_true;  ecg_T_wavedet_p(isnan(ecg_T_wavedet_p))=[];
-
-        ecg_Toff_true = true_position(ch).Toff';  ecg_Toff_wavedet_p = ecg_Toff_true;  ecg_Toff_wavedet_p(isnan(ecg_Toff_wavedet_p))=[];
-
 
         a = figure('Position', [130 130 1500 800]);
         lg = {};
@@ -60,26 +42,6 @@ for m = 1:length(local_db_files)
         hold on
         ecg_plot = ecg_denoised;
         plot(t_second(ecg_rpeaks_index_p),ecg_plot(ecg_rpeaks_index_p),'*',MarkerSize=6,LineWidth=1);lg = cat(2, lg, {'R'});
-
-        if ~isempty(ecg_Ton_wavedet_p)
-            plot(t_second(ecg_Ton_wavedet_p),ecg_plot(ecg_Ton_wavedet_p),'r+',MarkerSize=10,LineWidth=2);lg = cat(2, lg, {'EXPERT-Ton'});
-        end
-        if ~isempty(ecg_T_wavedet_p)
-            plot(t_second(ecg_T_wavedet_p),ecg_plot(ecg_T_wavedet_p),'r*',MarkerSize=10,LineWidth=2);lg = cat(2, lg, {'EXPERT-T'});
-        end
-
-        if ~isempty(ecg_Toff_wavedet_p)
-            plot(t_second(ecg_Toff_wavedet_p),ecg_plot(ecg_Toff_wavedet_p),'rx',MarkerSize=10,LineWidth=2);lg = cat(2, lg, {'EXPERT-Toff'});
-        end
-
-        if ~isempty(ecg_Pon_index_p)
-            plot(t_second(ecg_Pon_index_p),ecg_plot(ecg_Pon_index_p),'k+',MarkerSize=12,LineWidth=2);lg = cat(2, lg, {'Pon'});
-            plot(t_second(ecg_P_index_p),ecg_plot(ecg_P_index_p),'k*',MarkerSize=12,LineWidth=2);lg = cat(2, lg, {'P'});
-            plot(t_second(ecg_Poff_index_p),ecg_plot(ecg_Poff_index_p),'kx',MarkerSize=12,LineWidth=2);lg = cat(2, lg, {'Poff'});
-        end
-
-        plot(t_second(ecg_QRSon_wavdet_p),ecg_plot(ecg_QRSon_wavdet_p),'go',MarkerSize=10,LineWidth=2);lg = cat(2, lg, {'EXPERT-QRSon'});
-        plot(t_second(ecg_QRSoff_wavedet_p),ecg_plot(ecg_QRSoff_wavedet_p),'go',MarkerSize=10,LineWidth=2);lg = cat(2, lg, {'EXPERT-QRSoff'});
 
         grid on
         legend(lg,'Interpreter' ,'latex','orientation','horizontal','FontSize',14)
---
+++ make_local_lu_noisy.m
@@ -0,0 +1,72 @@
+
+clear;
+close all;
+clc;
+
+
+oset_path = 'D:\projects\toolboxes\OSET'; 'D:\my_projects\Alphanumeric\multimodal-cardiac-biomarkers\OSET';% enter the path of OSET toolbox
+addpath(genpath(oset_path))
+
+db_folder = '.\localLU';
+local_db_files = dir([db_folder '/*.mat']); % list of all mat files
+db_folder_save = '.\localLU_noisy';
+f50 = 50;
+
+for m = 1:length(local_db_files)
+
+    clc
+    close all
+
+    disp(m)
+
+    % load data and rpeaks
+    in_fname = local_db_files(m).name(1:end-4);
+
+    load([db_folder,'/',in_fname,'.mat']);
+
+ecg_noisy = ecg;
+    for ch = 1:12
+
+        index_R =  true_position(ch).R;
+        ecg_rpeaks_index = index_R;
+
+        ecg_denoised = ecg(:,ch)';
+        ecg_denoised = ecg_denoised - movmean(movmedian(ecg_denoised,[round(0.3*fs),round(0.3*fs)]),[round(0.15*fs),round(0.15*fs)]);
+        
+        baseline_wander = randn(1,length(ecg_denoised)+5*fs);
+        baseline_wander = lp_filter_zero_phase(baseline_wander, 0.1/fs);
+        baseline_wander = baseline_wander(5*fs+1:end);
+        baseline_wander = baseline_wander * std(ecg_denoised)/std(baseline_wander);
+
+        white_noise = randn(1,length(ecg_denoised));
+        white_noise = white_noise * std(ecg_denoised)/std(white_noise);
+
+        power_line = sin(2*pi*f50/fs*(1:length(ecg_denoised)+2*fs)+2*pi*rand(1));
+        baseline_50 = randn(1,length(ecg_denoised)+2*fs);
+        baseline_50 = lp_filter_zero_phase(baseline_50, 0.5/fs);
+        power_line = power_line.* abs(baseline_50);
+        power_line = power_line(2*fs+1:end);
+        power_line = power_line * std(ecg_denoised)/std(power_line);
+
+
+        emg_signal = randn(1,length(ecg_denoised)+2*fs);
+        emg_signal = sjk_eeg_filter(emg_signal, fs,20+randi(10),100+randi(50));
+
+        % emg_signal = lp_filter_zero_phase(emg_signal, 100/fs);
+        % emg_signal = emg_signal - lp_filter_zero_phase(emg_signal, 25/fs);
+        baseline_50 = randn(1,length(ecg_denoised)+2*fs);
+        baseline_50 = lp_filter_zero_phase(baseline_50, 0.2/fs);
+        emg_signal = emg_signal.* abs(baseline_50);
+        emg_signal = emg_signal(2*fs+1:end);
+        emg_signal = emg_signal * std(ecg_denoised)/std(emg_signal);
+
+        ecg_noisy(:,ch) = ecg_denoised + 0.25*emg_signal + 0.25*power_line + 0.1*white_noise + baseline_wander;
+        ecg(:,ch) = ecg_denoised;
+
+    end
+
+    save( [db_folder_save,'\',in_fname,'.mat'], 'ecg_noisy','ecg','t_second', 'true_position','fs')
+end
+
+
+
