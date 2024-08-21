cd8a1c298d83d99f9b1f743af116c072708518f2
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2024-08-21 12:18:55 +0330

    TV denoising was tested successfully

--- oset_denoising.m
+++ oset_denoising.m
@@ -5,3 +5,53 @@ clc;
 
 oset_path = 'D:\projects\toolboxes\OSET'; 'D:\my_projects\Alphanumeric\multimodal-cardiac-biomarkers\OSET';% enter the path of OSET toolbox
 addpath(genpath(oset_path))
+
+db_folder = '.\localLU_noisy';
+local_db_files = dir([db_folder '/*.mat']); % list of all mat files
+
+f50 = 50;
+
+for m = 1:length(local_db_files)
+
+    clc
+    close all
+
+    disp(m)
+
+    % load data and rpeaks
+    in_fname = local_db_files(m).name(1:end-4);
+
+    load([db_folder,'/',in_fname,'.mat']);
+
+    % ecg_noisy = ecg;
+    for ch = 1:12
+
+        index_R =  true_position(ch).R;
+        ecg_rpeaks_index = index_R;
+
+        ecg_in = ecg_noisy(:,ch)';
+
+       
+        % ecg_in = ecg_den_lti_smoother(ecg_in,2,100);
+        % ecg_in = ecg_in - movmean(movmedian(ecg_in,[round(0.3*fs),round(0.3*fs)]),[round(0.15*fs),round(0.15*fs)]);
+
+
+        params = [];
+        ecg_rpeaks = zeros(1,length(ecg_in));
+        ecg_rpeaks(ecg_rpeaks_index) = 1;
+        % [ecg_in, data_prior_est, n_var] = ecg_den_phase_domain_gp(ecg_in, ecg_rpeaks, params);
+         [ecg_in, data_prior_est, n_var] = ecg_den_time_domain_gp(ecg_in, ecg_rpeaks, params);
+
+        ecg_in = ecg_in - movmean(movmedian(ecg_in,[round(0.3*fs),round(0.3*fs)]),[round(0.15*fs),round(0.15*fs)]);
+
+        ecg_denoised = ecg(:,ch)' ;
+
+        plot(t_second,ecg_noisy(:,ch))
+        hold on
+        plot(t_second,ecg_in)
+        plot(t_second,ecg_denoised)
+
+    end
+
+
+end
